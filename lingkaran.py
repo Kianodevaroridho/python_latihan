5d9d2a5365fe4a6d316ab0b72b84d1f2c926bc02
= <=>
2024-10-10 15:07:22 +0700

    program lingkaran

---
+++ lingkaran.py
@@ -0,0 +1,15 @@
+# dibuat oleh : kiano devaro ridho
+# tanggal pengerjaan : 10 oktober 2024
+# program menghitung lingkaran
+
+print("="*40)
+print("LINGKARAN")
+print("="*40)
+
+r = float(input("masukan jarijari: "))
+
+l =lambda r : 3.14 * r * r
+k = lambda r : 2*3.14 * r
+
+print("luas :",l(r),'cm2')
+print("keliling :",round(k(r),2), 'cm2')
