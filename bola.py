bc20e505bd7781a27e8a434dfded8d9724de5b8e
= <=>
2024-10-22 09:20:35 +0700

    first commit

---
+++ bola.py
@@ -0,0 +1,17 @@
+#Dibuat Oleh : KIANO DEVARO RIDHO
+#Tanggal Pengerjaan: 22 Oktober 2024
+#Program Menghitung Rumus Bola
+
+print("="*40)
+print("PROGRAM BOLA")
+print("="*40)
+
+r = int(input("Jari-Jari\t: "))
+
+l = 4 * 3.14 * r * r
+v = 4/3 * 3.14 * r * r * r
+
+print(f"Luas\t\t: {l}")
+print(f"Value\t\t: {v}")
+
+
