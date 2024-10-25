fd8c83539e4dcb4692cb926b7196e0c2ebaa03b8
= <=>
2024-10-25 07:35:10 +0700

    first commit

---
+++ limassegitiga.py
@@ -0,0 +1,18 @@
+#DIBUAT OLEH:KIANO DEVARO RIDHO
+#Tanggal Pengerjaan: 25 Oktober 2024
+#Program menghitung Rumus Limas Segitiga
+
+print("="*40)
+print("Rumus Limas Segitiga")
+print("="*40)
+
+la = float(input("masukan luas alas limas: "))
+t = float(input("masukan tinggi limas: "))
+st = float(input("masukan luas sisi tegak limas: "))
+
+lp = la + st
+v = 1 / 3 8 la * t
+
+print(f'''Volume/t: {v}
+Luas Permukaan\t {lp}
+''')
