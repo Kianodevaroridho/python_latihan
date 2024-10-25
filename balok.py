af396f6aaf427baf4340318c52dbbf2ddf9fa1c7
= <=>
2024-10-25 07:43:57 +0700

    first commit

---
+++ balok.py
@@ -0,0 +1,19 @@
+#DIBUAT OLEH:KIANO DEVARO RIDHO
+#Tanggal Pengerjaan: 25 Oktober 2024
+#Program Menghitung Rumus Balok
+
+print("="*40)
+print("PROGRAM BALOK")
+print("="*40)
+
+p = int(input("Panjang\t\t: "))
+l = int(input("Lebar\t\t: "))
+t = int(input("Tinggi\t\t: "))
+
+lp = 2 * (p * + p * t + l * t)
+v = p * l * t 
+k = 4 * (p + l + t)
+
+print(f"Luas Permukaan\t: {lp}")
+print(f"Volume\t\t: {v}")
+print(f"Keliling\t: {k}")
