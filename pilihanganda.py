5b7bf7d804e747b8a58a876a0c8e8ed65e9c4118
= <=>
2024-11-07 14:03:34 +0700

    Pilihan Ganda

---
+++ pilihanganda.py
@@ -0,0 +1,16 @@
+print("SIAPA KIPER PEMAIN PERSIB BANDUNG SEKARANG")
+print('''
+    A.Adit
+    B.Rafi
+    C.Dika
+    D.Kevin Mendoza''')
+
+a=(input('Masukan Jawaban: '))
+if a == 'D':
+    print('Benar')
+else:
+    print ('Salah,Jawabanya adalah, D')
+
+
+
+
